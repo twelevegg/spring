88658825e9045a9aad8faf19f765f8403d6ec515
Ewan Jee <830jiforcareer@gmail.com>
2026-01-19 13:47:46 +0900

    refactor: Setter 제거

--- src/main/java/com/twelvegg/aicc/postgres/cdr/domain/Cdr.java
+++ src/main/java/com/twelvegg/aicc/postgres/cdr/domain/Cdr.java
@@ -6,12 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "cdrs")
 @Getter
-@Setter
 public class Cdr {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

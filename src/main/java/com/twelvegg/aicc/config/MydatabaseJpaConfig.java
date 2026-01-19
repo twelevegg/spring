fd0bee02869785ac37dbb77cad620d5fb101a9e1
Ewan Jee <830jiforcareer@gmail.com>
2026-01-19 15:52:12 +0900

    refactor: mysql -> mydatabase 로 참고 변경

---
+++ src/main/java/com/twelvegg/aicc/config/MydatabaseJpaConfig.java
@@ -0,0 +1,41 @@
+package com.twelvegg.aicc.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.twelvegg.aicc.mydatabase", entityManagerFactoryRef = "mysqlEntityManagerFactory", transactionManagerRef = "mysqlTransactionManager")
+public class MydatabaseJpaConfig {
+
+    @Primary
+    @Bean(name = "mysqlEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean mysqlEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("mysqlDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.twelvegg.aicc.mydatabase")
+                .persistenceUnit("mysql")
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "mysqlTransactionManager")
+    public PlatformTransactionManager mysqlTransactionManager(
+            @Qualifier("mysqlEntityManagerFactory") LocalContainerEntityManagerFactoryBean mysqlEntityManagerFactory) {
+        return new JpaTransactionManager(Objects.requireNonNull(mysqlEntityManagerFactory.getObject()));
+    }
+}

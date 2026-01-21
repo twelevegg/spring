0b087a93d624bdbaf3fd25985b7759a70521a6c8
Ewan Jee <830jiforcareer@gmail.com>
2026-01-21 15:51:34 +0900

    feat: Implement API, service, and data access layers for call, member, product, tenant, department, and customer entities, including exception handling.

---
+++ src/main/java/com/twelvegg/aicc/exception/CustomExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.twelvegg.aicc.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { CustomException.class })
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+}

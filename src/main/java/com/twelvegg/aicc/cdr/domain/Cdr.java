cfba107afa83f7a7f131745652f220924b745138
Ewan Jee <830jiforcareer@gmail.com>
2026-01-21 10:07:06 +0900

    feat: CDR 엔티티 칼럼 추가

--- src/main/java/com/twelvegg/aicc/cdr/domain/Cdr.java
+++ src/main/java/com/twelvegg/aicc/cdr/domain/Cdr.java
@@ -1,18 +1,41 @@
 package com.twelvegg.aicc.cdr.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "cdrs")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Cdr {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String callId;
+
+    private String linkedId;
+
+    private String src;
+
+    private String dst;
+
+    private LocalDateTime start;
+
+    private LocalDateTime answer;
+
+    private LocalDateTime end;
+
+    private Integer duration;
+
+    private Integer billsec;
+
+    private String disposition;
 }

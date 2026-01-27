94eb495b5590d34d242e253c7099b8d2344ca915
Ewan Jee <830jiforcareer@gmail.com>
2026-01-27 09:52:48 +0900

    feat: Tenant, Member 레포지토리 추가

--- src/main/java/com/twelvegg/aicc/mydatabase/member/repository/MemberRepository.java
+++ src/main/java/com/twelvegg/aicc/mydatabase/member/repository/MemberRepository.java
@@ -3,7 +3,9 @@ package com.twelvegg.aicc.mydatabase.member.repository;
 import com.twelvegg.aicc.mydatabase.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
